d5a332614aff8e52174b23655c49ef1c5d3babcc
Sarah Ruth <psarahruth@gmail.com>
2024-07-22 17:23:59 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+     <div>
+      <header>
+        TS amazona
+      </header>
+      <main>Sarah</main>
+      <footer>All Rights Reserved</footer>
+     </div>
+  )
+}
+
+export default App
